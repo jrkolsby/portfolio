ce8d43cb877afd970b73bede85d7178cae090a30
James Kolsby <jrkolsby@mac.com>
2015-05-07 00:25:37 -0400

    Sketchy jQuery margin fix

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+	$('article.small').each(function(i) {
+		console.log(i);
+		if (i%2 == 0) {
+			$(this).addClass('margin'); //This makes me feel dirty
+		}
+	});
 	$('section#about, main').addClass("top");
 	$(window).scroll(function() {
 		if ($(window).scrollTop() > 10 && $("section#about a:last-child").offset().top
